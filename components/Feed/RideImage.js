bc4b6e150f3ba26bb8329e5e6082f8c9a6bf01d3
Matt Berlin <matt@equesteo.com>
2018-10-03 10:23:16 -0700

    fix map images

--- components/Feed/RideImage.js
+++ components/Feed/RideImage.js
@@ -8,7 +8,7 @@ import { STATIC_MAPS_API_KEY } from 'react-native-dotenv'
 
 export default class RideImage extends PureComponent {
   render() {
-    const uriWithKey = this.props.uri + `&${STATIC_MAPS_API_KEY}`
+    const uriWithKey = this.props.uri + `&key=${STATIC_MAPS_API_KEY}`
     return (
       <Image
         source={{uri: uriWithKey}}
