3d640453dfe90c3ae62b3bd9c0ea9b98ec11aa36
Matt Berlin <matt@equesteo.com>
2018-12-26 16:28:04 -0800

    show push notifications for ride carrot

--- services/PushNotificationHandler.js
+++ services/PushNotificationHandler.js
@@ -14,5 +14,9 @@ export function handleNotification (dispatch, messageData, background) {
       const commentMessage = `${messageData.commenterName} commented: ${messageData.comment}`
       dispatch(showLocalNotification(commentMessage, background, commentRideID, true))
       return
+    case 'newCarrot':
+      const carrotRideID = messageData.carrotRideID
+      const carrotMessage = `${messageData.carroterName || 'Someone'} gave you a carrot!`
+      dispatch(showLocalNotification(carrotMessage, background, carrotRideID, false))
   }
 }
