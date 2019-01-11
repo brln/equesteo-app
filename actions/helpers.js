57097607290d1d8879b2ec0c32165a5dd32bb23c
Matt Berlin <matt@equesteo.com>
2019-01-11 14:28:58 -0800

    versioning

--- actions/helpers.js
+++ actions/helpers.js
@@ -15,13 +15,12 @@ import {
 import {
   startListeningFCMTokenRefresh,
   startListeningFCM,
-  syncDBPull,
   setDistributionOnServer,
   switchRoot,
 } from './functional'
 import { logError, logInfo } from '../helpers'
 import { FEED } from '../screens'
-import { LocalStorage } from '../services'
+import { LocalStorage, PouchCouch } from '../services'
 
 import { setUserContext } from "../services/Sentry"
 
