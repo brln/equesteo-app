7a2af6565f41c4a22457fc350c40b1b266bc141a
Matt Berlin <matt@equesteo.com>
2019-04-22 07:40:49 -0700

    save

---
+++ components/Shared/HorseThumbnails.js
@@ -0,0 +1,77 @@
+import React, { PureComponent } from 'react'
+import {
+  Dimensions,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native'
+
+import Thumbnail from '../../components/Images/Thumbnail'
+import { orange, darkGrey } from '../../colors'
+
+const { width } = Dimensions.get('window')
+
+export default class HorseThumbnails extends PureComponent {
+  constructor (props) {
+    super(props)
+  }
+
+  horseProfileURL (horse, horsePhotos) {
+    const profilePhotoID = horse.get('profilePhotoID')
+    if (horse && profilePhotoID && horsePhotos.get(profilePhotoID)) {
+      return horsePhotos.getIn([profilePhotoID, 'uri'])
+    }
+  }
+
+  showHorseProfile (horse) {
+    return () => {
+      this.props.showHorseProfile(horse)
+    }
+  }
+
+  oneHorse (horse) {
+    const horseProfileURL = this.horseProfileURL(horse, this.props.horsePhotos)
+    return (
+      <TouchableOpacity
+        key={horse.get('_id')}
+        style={{flex: 1, alignItems: 'flex-end', justifyContent: 'center'}}
+        onPress={this.showHorseProfile(horse)}
+      >
+        <View style={{flex: 1, alignItems: 'center'}}>
+          <Text numberOfLines={1} style={{color: darkGrey, fontSize: 12, textAlign: 'center'}}>{horse.get('name')}</Text>
+          <Thumbnail
+            source={{uri: horseProfileURL}}
+            emptySource={require('../../img/breed.png')}
+            empty={!horseProfileURL}
+            height={width / 10}
+            width={width / 10}
+            round={true}
+            borderColor={horse.get('color') || null}
+          />
+        </View>
+      </TouchableOpacity>
+    )
+  }
+
+  andMore () {
+    if (this.props.horses.count() > 2) {
+      return (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <Text style={{fontSize: 18}}>+{this.props.horses.count() - 2}</Text>
+        </View>
+      )
+    } else {
+      return null
+    }
+  }
+
+  render () {
+    const twoHorses = this.props.horses.slice(0, 2).map(h => this.oneHorse(h))
+    return (
+      <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-end'}}>
+        { twoHorses }
+        { this.andMore() }
+      </View>
+    )
+  }
+}
