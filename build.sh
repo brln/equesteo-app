0c7f1d045b3dbf45c88fa1b9fb65f07bdfcaf9e6
Matt Berlin <matt@equesteo.com>
2019-04-30 09:17:31 -0700

    save

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-# cd android && ./gradlew publishApkRelease
+#! /bin/bash
+cd android && ./gradlew publishApkRelease;
+
 cd ios && xcodebuild -workspace equesteo.xcworkspace -scheme equesteoRelease -configuration AppStoreDistribution archive -archivePath ./build/equesteo.xcarchive
 xcodebuild -exportArchive -archivePath ./build/equesteo.xcarchive -exportOptionsPlist prodBuild.plist -exportPath ./build;
 /Applications/Xcode.app/Contents/Applications/Application\ Loader.app/Contents/Frameworks/ITunesSoftwareService.framework/Support/altool --upload-app -f "./build/equesteo.ipa" -u $USERNAME -p $PASSWORD;
