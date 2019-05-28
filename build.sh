543a20541a6f41b1b57eddbce221632bcc3adb23
Matt Berlin <matt@equesteo.com>
2019-05-28 10:15:18 -0700

    save

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 set -o pipefail
 
 cd ios && rm -rf build && rm -rf ~/Library/Developer/Xcode/DerivedData &&  xcodebuild -workspace equesteo.xcworkspace -scheme equesteoRelease -configuration AppStoreDistribution archive -archivePath ./build/equesteo.xcarchive;
-xcodebuild -exportArchive -archivePath ./build/equesteo.xcarchive -exportOptionsPlist prodBuild.plist -exportPath ./build;
+cd ios && xcodebuild -exportArchive -archivePath ./build/equesteo.xcarchive -exportOptionsPlist prodBuild.plist -exportPath ./build;
 /Applications/Xcode.app/Contents/Applications/Application\ Loader.app/Contents/Frameworks/ITunesSoftwareService.framework/Support/altool --upload-app -f "./build/equesteo.ipa" -u $APP_STORE_USERNAME -p $APP_STORE_PASSWORD;
 
-cd ../android && ./gradlew publishApkRelease;
+# cd ../android && ./gradlew publishApkRelease;
